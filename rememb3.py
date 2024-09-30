843030eedf85be0d9b8a9e259ba101b3cbd488c9
igrekon <igorechekneznaika@gmail.com>
2024-09-30 17:26:48 +0300

    Add file

---
+++ rememb3.py
@@ -0,0 +1,12 @@
+user_data = int(input('Введите число: '))
+
+isHappy = False
+
+if user_data or isHappy == 5:
+    print('Мы на месте')
+    if user_data > 6:
+        print("Number is bigger than 5")
+    elif user_data == 5:
+        print('Number is 5')
+    else:
+        print('User is unhappy.')
